c077c747ea84ad975212a9b9c99ecd644cf0f6a0
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-06-02 21:51:52 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+    },
+    fontSize: {
+      base: '12px'
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
+
